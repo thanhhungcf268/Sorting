4f587398128207458b2bf77502b3c954e8b8a16f
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-04-22 16:49:03 +0700

    [Bài tập] Cài đặt thuật toán sắp xếp chèn

---
+++ InsertionSort.java
@@ -0,0 +1,19 @@
+public class InsertionSort {
+    public int[] insertionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minIndex = i;
+            for (int j = 1 + i; j < array.length; j++) {
+                if (min > array[j]) {
+                    min = array[j];
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                array[minIndex] = array[i];
+                array[i] = min;
+            }
+        }
+        return array;
+    }
+}
